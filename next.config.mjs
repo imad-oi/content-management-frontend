dbafa12d5684c349b838e78eb3594bb86001fbdc
imad oissafe <oissafe.imad@gmail.com>
2024-08-04 12:47:29 +0100

    implement securiy and fix BroadcastChannel lifecycle and add database clear functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'X-Frame-Options',
+                value: 'DENY',
+              },
+              {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff',
+              },
+              {
+                key: 'Referrer-Policy',
+                value: 'strict-origin-when-cross-origin',
+              },
+              {
+                key: 'Content-Security-Policy',
+                value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';",
+              },
+            ],
+          },
+        ];
+      },
+};
 
 export default nextConfig;
